b8a883d2412a7988c328f4e295fa67c6290a9554
mjspicuzza <113363435+mjspicuzza@users.noreply.github.com>
2024-06-13 17:15:26 -0500

    lookup 2

--- streamlit_app.py
+++ streamlit_app.py
@@ -30,7 +30,7 @@ if ingredients_list:
     for fruit_chosen in ingredients_list:
         ingredients_string += fruit_chosen + ' '
         st.subheader(fruit_chosen + ' Nutrition Information')
-        df = cnx.query("select SEARCH_ON from smoothies.public.fruit_options WHERE FRUIT_NAME = {fruit_chosen}")
+        df = cnx.query(f"select SEARCH_ON from smoothies.public.fruit_options WHERE FRUIT_NAME = {fruit_chosen}")
         value = df.first()
         fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + value)
         fv_df = st.dataframe(data=fruityvice_response.json(), use_container_width=True)
